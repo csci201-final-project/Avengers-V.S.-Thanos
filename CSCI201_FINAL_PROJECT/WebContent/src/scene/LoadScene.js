ac8ed17d6f553b8b44a7326cb95009ffa20ba8c5
Lihan Zhu <lihanzhu@usc.edu>
2019-04-14 14:24:13 -0700

    update loading bar

--- CSCI201_FINAL_PROJECT/WebContent/src/scene/LoadScene.js
+++ CSCI201_FINAL_PROJECT/WebContent/src/scene/LoadScene.js
@@ -23,7 +23,7 @@ export class LoadScene extends Phaser.Scene {
    		var progressBar = this.add.graphics();
         var progressBox = this.add.graphics();
         progressBox.fillStyle(0x222222, 0.8);
-        progressBox.fillRect(240, 270, 320, 50);
+        progressBox.fillRect(240, 270, 820, 50);
         
         var width = this.cameras.main.width;
         var height = this.cameras.main.height;
@@ -69,7 +69,7 @@ export class LoadScene extends Phaser.Scene {
             percentText.setText(parseInt(value * 100) + '%');
             progressBar.clear();
             progressBar.fillStyle(0xffffff, 1);
-            progressBar.fillRect(250, 280, 300 * value, 30);
+            progressBar.fillRect(250, 280, 800 * value, 30);
         });
         
         this.load.on('fileprogress', function (file) {
